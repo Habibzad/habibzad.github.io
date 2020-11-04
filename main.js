81e31e7643e95ad695735ef4261cd76bfeec8724
Habibzad <masoud.habibzad@gmail.com>
2020-11-03 23:32:24 -0500

    add new theme

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-$(document).ready(function(){
-
-    $("#btn").click(function(){
-
-      if($("#btn").text() == "☰"){
-        $("#btn").text("🞬");
-      }else{
-        $("#btn").text("☰");
-      }
-
-      $("li").toggle("slow");
-    });
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", ()=>{
+    if(!menuOpen){
+        menuBtn.classList.add("open");
+        navList.classList.add('nav-open');
+        menuOpen = true;
+    }else{
+        menuBtn.classList.remove("open");
+        navList.classList.remove('nav-open');
+        menuOpen = false;
+    }
 });
+
+
+const navLink = document.querySelectorAll('.nav-link');
+let navList = document.querySelector(".nav-list");
+
+navLink.forEach(link=>{
+    link.addEventListener("click", ()=>{
+        navList.classList.remove('nav-open'); 
+        menuBtn.classList.remove("open");
+        menuOpen = false;
+    })
+})
