5a19d6c5f89315e7d1de8724c85fbc65e4d042f6
Ahmad Habibzad <masoud.habibzad@gmail.com>
2020-12-23 19:18:19 -0500

    new commit

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -23,35 +23,3 @@ navLink.forEach(link=>{
         menuOpen = false;
     })
 })
-
-
-// Text Animation
-
-document.addEventListener('DOMContentLoaded',function(event){
-    const dataText = [ "I like tea", "I like sunny days", "I like coding", "I like swimming"];
-    function typeWriter(text, i, fnCallback) {
-      if (i < (text.length)) {
-       document.querySelector("#likes").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
-  
-        setTimeout(function() {
-          typeWriter(text, i + 1, fnCallback)
-        }, 100);
-      }
-      else if (typeof fnCallback == 'function') {
-        setTimeout(fnCallback, 1000);
-      }
-    }
-     function StartTextAnimation(i) {
-       if (typeof dataText[i] == 'undefined'){
-          setTimeout(function() {
-            StartTextAnimation(0);
-          }, 1000);
-       }
-      if (i < dataText[i].length) {
-       typeWriter(dataText[i], 0, function(){
-         StartTextAnimation(i + 1);
-       });
-      }
-    }
-    StartTextAnimation(0);
-  });
