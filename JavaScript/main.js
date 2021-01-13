ab2722ea871cbad6766f885500b3440323331f63
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-01-12 23:35:57 -0500

    New features

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,11 +1,11 @@
 const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
-menuBtn.addEventListener("click", ()=>{
-    if(!menuOpen){
+menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
         menuBtn.classList.add("open");
         navList.classList.add('nav-open');
         menuOpen = true;
-    }else{
+    } else {
         menuBtn.classList.remove("open");
         navList.classList.remove('nav-open');
         menuOpen = false;
@@ -16,10 +16,39 @@ menuBtn.addEventListener("click", ()=>{
 const navLink = document.querySelectorAll('.nav-link');
 let navList = document.querySelector(".nav-list");
 
-navLink.forEach(link=>{
-    link.addEventListener("click", ()=>{
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
         navList.classList.remove('nav-open');
         menuBtn.classList.remove("open");
         menuOpen = false;
     })
 })
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    var dataText = ["I like tea", "I like sunny days", "I like coding", "I like swimming"];
+    function typeWriter(text, i, fnCallback) {
+        if (i < (text.length)) {
+            document.querySelector("#likes").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+            setTimeout(function () {
+                typeWriter(text, i + 1, fnCallback)
+            }, 100);
+        }
+        else if (typeof fnCallback == 'function') {
+            setTimeout(fnCallback, 1000);
+        }
+    }
+    function StartTextAnimation(i) {
+        if (typeof dataText[i] == 'undefined') {
+            setTimeout(function () {
+                StartTextAnimation(0);
+            }, 1000);
+        }
+        if (i < dataText[i].length) {
+            typeWriter(dataText[i], 0, function () {
+                StartTextAnimation(i + 1);
+            });
+        }
+    }
+    StartTextAnimation(0);
+});
